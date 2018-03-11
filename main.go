4207b1787a70fc89728b1eca97b5560be9c6bb2a
Jian Han <jianhan@mail.com>
2018-03-11 10:49:49 +1000

    [DEV] Start to put things together in main first

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"context"
+
+	"github.com/google/go-github/github"
+	"github.com/jianhan/shopify_repos/api"
 	"github.com/jianhan/shopify_repos/handlers"
+	"golang.org/x/oauth2"
 )
 
 func main() {
-	handlers.Serve("127.0.0.1:8080")
+	// Get fetcher
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		// TODO: following token needed to be in ENV
+		&oauth2.Token{AccessToken: "f4d3f4021a69bd58d30fc0cd4b31698233a59ba3"},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+	// TODO: options is nil now, may needed to be changes later if pagination needed to be implemented.
+	spfFetcher := api.NewShopify("Shopify", client, nil)
+
+	// TODO: address and port should be in ENV
+	handlers.Serve("127.0.0.1:8080", spfFetcher)
 }
